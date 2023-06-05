195568d8bba6eecc7c06173fa41092ec3317dd2d
CRoth23 <carterroth123@gmail.com>
2023-06-05 18:47:52 -0400

    Naming files

--- src/main/java/com/specsounds/SpecSoundsConfig.java
+++ src/main/java/com/specsounds/SpecSoundsConfig.java
@@ -1,11 +1,11 @@
-package com.example;
+package com.specsounds;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("example")
-public interface ExampleConfig extends Config
+public interface SpecSoundsConfig extends Config
 {
 	@ConfigItem(
 		keyName = "greeting",
--- src/main/java/com/specsounds/SpecSoundsPlugin.java
+++ src/main/java/com/specsounds/SpecSoundsPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.specsounds;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -16,13 +16,13 @@ import net.runelite.client.plugins.PluginDescriptor;
 @PluginDescriptor(
 	name = "Example"
 )
-public class ExamplePlugin extends Plugin
+public class SpecSoundsPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private SpecSoundsConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	SpecSoundsConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(SpecSoundsConfig.class);
 	}
 }
